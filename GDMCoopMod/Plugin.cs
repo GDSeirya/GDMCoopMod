fa27ff50a1bdb11ebc47eb1821322eb1b6c2d361
GD Seirya <rubyprojectgoblin@hotmail.com>
2026-08-16 14:30:52 +1000

    Update version number

--- GDMCoopMod/Plugin.cs
+++ GDMCoopMod/Plugin.cs
@@ -7,7 +7,7 @@ using HarmonyLib;
 using Il2CppInterop.Runtime.Injection;
 using UnityEngine;
 
-[BepInPlugin("gdm.coopmod", "GDM Coop Plugin", "1.6.6")]
+[BepInPlugin("gdm.coopmod", "GDM Coop Plugin", "1.7.0")]
 public class GDMCoopPlugin : BasePlugin
 {
     public static ManualLogSource StaticLog;
@@ -95,7 +95,7 @@ public class GDMCoopPlugin : BasePlugin
         AssignConfiguredController(3,player4Character.Value);
 
 
-        StaticLog.LogInfo("GDM Coop Plugin loaded v1.6.6");
+        StaticLog.LogInfo("GDM Coop Plugin loaded v1.7.0");
     }
 
 
